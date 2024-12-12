0986dd83233b5ab4c8512a2ce417ef7ad7ab922a
Brandon Shega <b.shega@gmail.com>
2024-12-11 22:49:58 -0500

    Making sketchybar look pretty

---
+++ private_dot_config/sketchybar/plugins/executable_tailscale.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# Toggles between tailscale being on and off.
+
+update_button() {
+  if [ "$RUNNING" = true ]; then
+    sketchybar --set "$NAME" label="VPN ON" icon.drawing=on icon= background.color=0xffB3E1A7
+  else
+    sketchybar --set "$NAME" label="VPN OFF" icon.drawing=off background.color=0xffef9f76
+  fi
+}
+
+STATUS=$(tailscale status)
+RUNNING=false
+
+if ! [ "$STATUS" = "Tailscale is stopped." ]; then
+  RUNNING=true
+fi
+
+update_button
+
+if [ "$SENDER" = "tailscale" ]; then
+  if [ "$RUNNING" = true ]; then
+    tailscale down
+    RUNNING=false
+    update_button
+  else
+    tailscale up
+    RUNNING=true
+    update_button
+  fi
+fi
