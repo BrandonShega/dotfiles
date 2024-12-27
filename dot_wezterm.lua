1b8ec1bc766b6d4b83d0f3ef5ffe726c81ca2b20
Brandon Shega <b.shega@gmail.com>
2024-12-27 12:22:54 -0500

    Changing some theming things

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -1,15 +1,10 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
-local act = wezterm.action
 
 config.color_scheme = "Catppuccin Frappe"
-
 config.font = wezterm.font("MonaspiceNe Nerd Font")
-
+config.font_size = 14
 config.window_decorations = "RESIZE"
-
 config.enable_tab_bar = false
-
-config.default_prog = { "/usr/bin/zsh" }
 
 return config
