ceed4b348445c01358dbca0bb1b112d15ff516f0
Brandon Shega <b.shega@gmail.com>
2026-09-18 08:48:13 -0400

    Map Neovim config subfiles into ~/.config/nvim to keep ~/.config/nvim directory writable for lazy-lock.json

--- modules/shared/nvim.nix
+++ modules/shared/nvim.nix
@@ -5,6 +5,9 @@
     neovim
   ];
 
-  # Declaratively install Neovim configuration into ~/.config/nvim
-  xdg.configFile."nvim".source = ../../config/nvim;
+  # Map Neovim config subfiles into ~/.config/nvim while keeping the directory writable for lazy-lock.json
+  xdg.configFile."nvim/init.lua".source = ../../config/nvim/init.lua;
+  xdg.configFile."nvim/lua".source = ../../config/nvim/lua;
+  xdg.configFile."nvim/lazyvim.json".source = ../../config/nvim/lazyvim.json;
+  xdg.configFile."nvim/stylua.toml".source = ../../config/nvim/stylua.toml;
 }
